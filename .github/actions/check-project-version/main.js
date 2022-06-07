be4074cf966bd558e3c9a00d16d12116800ddecd
Ricardo Martín <ricardomartin@playtomic.io>
2022-06-07 17:09:46 +0200

    Set outputs

--- .github/actions/check-project-version/main.js
+++ .github/actions/check-project-version/main.js
@@ -1,17 +1,21 @@
 const semver = require("semver");
+const core = require("@actions/core");
 
-function getInput(name) {
-  let val = process.env[`INPUT_${name.replace(/ /g, "_").toUpperCase()}`] || "";
-  val = val.trim();
-
-  return val;
-}
+// function getInput(name) {
+//   let val = process.env[`INPUT_${name.replace(/ /g, "_").toUpperCase()}`] || "";
+//   val = val.trim();
+//   return val;
+// }
 
 (function main() {
-  let releaseType = getInput("release-type");
-  const version = getInput("version");
-  console.log("Type of release: ", releaseType);
-  console.log("Version: ", version);
-  releaseType = releaseType == "hotfix" ? "patch" : releaseType;
-  console.log(semver.inc(version, releaseType));
+  try {
+    let releaseType = core.getInput("release-type");
+    console.log("Release Type: ", releaseType);
+    releaseType = releaseType == "hotfix" ? "patch" : releaseType;
+    const version = core.getInput("version");
+    const newVersion = semver.inc(version, releaseType);
+    core.setOutput("new_version", newVersion);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 })();
