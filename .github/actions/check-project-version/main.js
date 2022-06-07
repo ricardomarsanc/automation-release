0b1a7e28eb23321f69c013562aa359395d805bc0
Ricardo Martín <ricardomartin@playtomic.io>
2022-06-07 16:53:51 +0200

    added version check

--- .github/actions/check-project-version/main.js
+++ .github/actions/check-project-version/main.js
@@ -8,12 +8,10 @@ function getInput(name) {
 }
 
 (function main() {
-  const releaseType = getInput("release-type");
+  let releaseType = getInput("release-type");
   const version = getInput("version");
   console.log("Type of release: ", releaseType);
   console.log("Version: ", version);
-  console.log("release variable type: ", typeof releaseType);
-  console.log("Version type: ", typeof version);
   releaseType = releaseType == "hotfix" ? "patch" : releaseType;
   console.log(semver.inc(version, releaseType));
 })();
